d888d01b05e0cb22b6c3755fd5396fa1bcf976ac
Hippolyte Barraud <hippolyte.barraud@datadoghq.com>
2023-09-09 22:32:12 -0400

    grpc: return encoded message buffers to buffer pool, if any
    Provide a callback to the transport layer that inserts the encoded
    message buffer to the buffer pool once the message has been copied or
    sent over the wire and no longer referenced anywhere.
    
    Note that the encoded message buffer is also shared with user code
    through handlers. Those should not keep references to these buffers
    after they return.

--- server.go
+++ server.go
@@ -1140,6 +1140,14 @@ func (s *Server) sendResponse(ctx context.Context, t transport.ServerTransport, 
 	if len(payload) > s.opts.maxSendMessageSize {
 		return status.Errorf(codes.ResourceExhausted, "grpc: trying to send message larger than max (%d vs. %d)", len(payload), s.opts.maxSendMessageSize)
 	}
+
+	encoderBufferPool := s.opts.encoderBufferPool
+	if encoderBufferPool != nil {
+		opts.OnWrittenToTransport = func() {
+			encoderBufferPool.Put(&data)
+		}
+	}
+
 	err = t.Write(stream, hdr, payload, opts)
 	if err == nil {
 		for _, sh := range s.opts.statsHandlers {
